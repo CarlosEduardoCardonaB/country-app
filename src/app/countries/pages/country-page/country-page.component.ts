570ac9591474ede31e609a7889205148b94ed22c
Carlos Eduardo <caeca425@gmail.com>
2024-09-19 21:36:33 -0500

    se agrega página info país solo y se crean subscripciones en country.service.ts

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/country.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -9,24 +11,54 @@ import { CountriesService } from '../../services/country.service';
 })
 export class CountryPageComponent implements OnInit {
 
+  public country1?: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
-    private countriesService: CountriesService
+    private countriesService: CountriesService,
+    private router: Router
    ){}
 
   ngOnInit(): void {
-    this.activatedRoute.params
-    .subscribe(
-      params => {
+    //aqui con este this.activatedRoute.params lo que hacemos es capturar el parámetro que nos llega en la URL y poder manipularlo.
+    //La url de este componente esta en country-routing.module.ts como path: 'by/:id'
 
-          this.countriesService.searchCountryByAlphaCode(params['id'])
-          .subscribe( country => {
-            console.log({country});
-          }
-        );
-        //console.log({params: params['id']})
-      }
+    //Esta es la forma de obtener la data con switchmap. (Esta y la de abajo funcionan similar)
+    // this.activatedRoute.params
+    // .pipe(
+    //   switchMap(( params ) => this.countriesService.searchCountryByAlphaCode( params['id'])),
+    // )
+    // .subscribe( ( country ) => {
+    //       console.log( country );
+    //   }
+    // );
+
+
+    //Esta es la segunda forma de obtener la info sin switchmap
+    // this.activatedRoute.params
+    // .subscribe( ( params ) => {
+
+    //       this.countriesService.searchCountryByAlphaCode(params['id'])
+    //       .subscribe( country => {
+    //         console.log({country});
+    //       }
+    //     );
+    //   }
+    // );
+
+
+    //ejemplo 1 pero con desestructuración del objeto params que se hace en {id}
+    this.activatedRoute.params
+    .pipe(
+      switchMap(( {id} ) => this.countriesService.searchCountryByAlphaCode( id)),
     )
+    .subscribe( ( country ) => {
+          if( !country ) return this.router.navigateByUrl('');
+          //console.log( country );
+          return this.country1 = country;
+       }
+    );
+
   }
 
 }
--- src/app/countries/services/country.service.ts
+++ src/app/countries/services/country.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -10,9 +10,14 @@ export class CountriesService {
 
   constructor(private httpClient: HttpClient) {}
 
-  searchCountryByAlphaCode(code: string): Observable<Country[]>{
+  searchCountryByAlphaCode(code: string): Observable<Country | null> {
     const url = `${ this.apiUrl }/alpha/${ code }`;
-    return this.getPetition(url);
+    //return this.getPetition(url);
+    return this.httpClient.get<Country[]>( url )
+    .pipe(
+      map( countries => countries.length > 0 ? countries[0]: null),
+      catchError( error => of(null))
+    );
   }
 
   searchCapital( term: string ): Observable<Country[]> {
@@ -34,7 +39,8 @@ export class CountriesService {
   }
 
   getPetition (url: string): Observable<Country[]>{
-    return this.httpClient.get<Country[]>( url ).pipe(
+    return this.httpClient.get<Country[]>( url )
+    .pipe(
       catchError( error => of([]))
     );
     //el pipe() es un operador de rxjs (librerias para manejar reactividad)
